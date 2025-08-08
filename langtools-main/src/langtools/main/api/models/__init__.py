18c5fd8aab0aa0858ae139573b8134f89fff33f8
Egor koldasov <koldasov3@gmail.com>
2025-08-08 16:13:32 +0700

    Revert "Dictionaty entry creation endpoint."
    This reverts commit 91c813440c0e609a20db73a7e726bd8a1037bc6c.

--- langtools-main/src/langtools/main/api/models/__init__.py
+++ langtools-main/src/langtools/main/api/models/__init__.py
@@ -4,20 +4,10 @@ from .auth_user import AuthUser
 from .profile import Profile
 from .auth_password import AuthPassword
 from .otp import OTP
-from .dictionary import (
-    DictionaryEntry,
-    UserDictionaryEntry,
-    DictionaryEntryTranslation,
-    MeaningFSRS,
-)
 
 __all__ = [
     "AuthUser",
     "Profile",
     "AuthPassword",
     "OTP",
-    "DictionaryEntry",
-    "UserDictionaryEntry",
-    "DictionaryEntryTranslation",
-    "MeaningFSRS",
 ]
