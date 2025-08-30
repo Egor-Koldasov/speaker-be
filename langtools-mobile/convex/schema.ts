7bb393625e90d50d32c1c4b5ffb585f57f93fb1d
Egor koldasov <koldasov3@gmail.com>
2025-08-30 12:10:21 +0700

    Add dictionary entry schemas

--- langtools-mobile/convex/schema.ts
+++ langtools-mobile/convex/schema.ts
@@ -1,6 +1,19 @@
 // import { authTables } from "@convex-dev/auth/server";
-import { defineSchema } from 'convex/server'
+import { defineSchema, defineTable } from 'convex/server'
+import { v } from 'convex/values'
 
 export default defineSchema({
   // ...authTables,
+  dictionaryEntries: defineTable({
+    updatedAt: v.optional(v.string()),
+    userId: v.string(),
+  }).index('byUserId', ['userId']),
+  dictionaryEntrySenses: defineTable({
+    updatedAt: v.optional(v.string()),
+    dictionaryEntryId: v.id('dictionaryEntries'),
+    localId: v.string(),
+    canonicalForm: v.string(),
+    definition: v.string(),
+    partOfSpeech: v.string(),
+  }).index('byDictionaryEntryIdLocalId', ['dictionaryEntryId', 'localId']),
 })
